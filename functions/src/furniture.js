80bae0bd6c3f5c76be4da02deea5dfb122d59120
Brad Jones <BJ2044@gmail.com>
2022-10-20 15:40:06 -0400

    Updates

--- functions/src/furniture.js
+++ functions/src/furniture.js
@@ -13,3 +13,20 @@ export async function getAllFurniture(req, res) {
     // send back the array of furniture
     res.send(collection)
 }
+
+export async function addNewFurniture(req, res) {
+    // create new furniture from the body of the request
+    const { name, type, color } = req.body
+    const newFurniture = { name, type, color }
+    // connect to the database
+    const db = dbConnect()
+    // put this new furniture into our furniture collection in our db
+    await db.collection('furniture').insertOne(newFurniture)
+    // catch errors and send w/status 500
+        .catch(err => {
+            res.status(500).send(err)
+            return
+        })
+    // return response with 201 all bueno
+    res.status(201).send({ message: 'Furniture added' })
+}
